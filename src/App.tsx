067e5089b2ac7199f02360b4935391460a603986
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-10-09 11:59:31 +0900

    環境構築をしてtremorの確認した

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Card, Metric, Text } from "@tremor/react";
+
+function App() {
+  return (
+    <>
+      <Card className="max-w-xs mx-auto">
+        <Text>Sales</Text>
+        <Metric>$ 34,743</Metric>
+      </Card>
+    </>
+  );
+}
+
+export default App;
